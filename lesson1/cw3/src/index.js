4568156f89ee60ea026079b554884e78c8462437
Artem Trusov <tavikingwork@gmail.com>
2023-05-03 21:45:33 +0300

    lesson 1 / classwork 3

---
+++ lesson1/cw3/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div class="greeting">
+    <h1 class="greeting__title">Hello, world!</h1>
+    <p class="greeting__text">I'am learning React</p>
+  </div>
+);
+
+ReactDOM.render(element, rootElement);
