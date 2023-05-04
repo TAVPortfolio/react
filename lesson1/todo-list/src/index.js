f1d5e7230742e6b65e3483b126a6991edf6ad332
Artem Trusov <tavikingwork@gmail.com>
2023-05-04 14:51:11 +0300

    lesson 1 / homework 1

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElement = document.getElementById('root');
+
+const titleElement = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(titleElement, rootElement);
