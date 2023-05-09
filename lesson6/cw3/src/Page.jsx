0f7693e66d3d024207605ea4723b18b1327aacfb
Artem Trusov <tavikingwork@gmail.com>
2023-05-09 23:19:27 +0300

    fixed

--- lesson6/cw3/src/Page.jsx
+++ lesson6/cw3/src/Page.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Info from './Info';
 
 const text1 = 'Price is 500$. Available in 2 colors';
-const text2 = 'Price is 650$. Not available';
+const text2 = 'Price is 650$. Not available in 1 colors';
 
 class Page extends Component {
   constructor(props) {
