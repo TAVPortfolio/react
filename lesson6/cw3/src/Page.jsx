01c2a0247a6599972dd43bab497e3dcd68adda11
Artem Trusov <tavikingwork@gmail.com>
2023-05-09 23:11:20 +0300

    lesson 6 / classwork 3

---
+++ lesson6/cw3/src/Page.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Info from './Info';
+
+const text1 = 'IPhone 13 - Price is 500$. Available in 2 colors';
+const text2 = 'Price is 650$. Not available';
+
+class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null,
+    };
+  }
+  setText = (text) => {
+    this.setState({
+      text,
+    });
+  };
+  render() {
+    return (
+      <div className="page">
+        <Info text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText(text1)}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText(text2)}>
+            Text 2
+          </button>
+          <button className="btn" onClick={() => this.setText('')}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
