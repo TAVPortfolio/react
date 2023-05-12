889ddecf4a9044b0a27214a9681f36170741151c
Artem Trusov <tavikingwork@gmail.com>
2023-05-12 10:36:46 +0300

    fixed

--- lesson6/cw3/src/Page.jsx
+++ lesson6/cw3/src/Page.jsx
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
-import Message from './Message';
+import Info from './Info';
 
-const text1 = 'Hello, world!';
-const text2 = 'Another exciting text.';
+const text1 = 'Price is 500$. Available in 2 colors';
+const text2 = 'Price is 650$. Not available in 1 color';
 
 class Page extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: null };
+    this.state = {
+      text: null,
+    };
   }
-
   setText = (text) => {
     this.setState({
       text,
     });
   };
-
   render() {
     return (
       <div className="page">
-        <Message text={this.state.text} />
+        <Info text={this.state.text} />
         <div className="actions">
           <button className="btn" onClick={() => this.setText(text1)}>
-            Text 1
+            IPhone 13
           </button>
           <button className="btn" onClick={() => this.setText(text2)}>
-            Text 2
+            IPhone 13 Pro
           </button>
           <button className="btn" onClick={() => this.setText('')}>
             Clear
