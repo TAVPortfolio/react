6e54ef9ae51c84d4c25de69cca96b6b7a2635e78
Artem Trusov <tavikingwork@gmail.com>
2023-05-15 14:11:19 +0300

    lesson 8 / classwork 2

--- lesson8/cw2/src/Clock.jsx
+++ lesson8/cw2/src/Clock.jsx
@@ -22,11 +22,15 @@ class Clock extends Component {
     };
   }
   componentDidMount() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.setState({
         time: moment(getTimeWithOffset(this.props.offset)).format('HH:mm:ss A'),
       });
     }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
