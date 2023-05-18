2aee559dc9af2d1c1817d5b6ea074605423ed1f5
Artem Trusov <tavikingwork@gmail.com>
2023-05-18 10:48:55 +0300

    lesson 8 / classwork 3

---
+++ lesson8/cw3/src/Life.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Life extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log('constructor: good place to create state');
+  }
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
+  shouldComponentUpdate(nextProps) {
+    console.log(
+      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+    );
+    return nextProps !== 0;
+  }
+  componentDidUpdate() {
+    console.log(
+      'componentDidUpdate(prevProps, prevState): some updates based on new props'
+    );
+  }
+  componentWillUnmount() {
+    console.log(
+      'componentWillUnmount(): cleanup before DOM related to component will be removed'
+    );
+  }
+
+  render() {
+    console.log('return React element to build DOM');
+    return <div>{this.props.number}</div>;
+  }
+}
+
+export default Life;
