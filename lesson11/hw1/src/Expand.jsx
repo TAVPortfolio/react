826110d3264bd45c2e42371abc35bbc0eae415d0
Artem Trusov <tavikingwork@gmail.com>
2023-05-26 14:31:22 +0300

    lesson 11 / homework 1

---
+++ lesson11/hw1/src/Expand.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+class Expand extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggleBtn = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleBtn}>
+            {this.state.isOpen ? (
+              <FontAwesomeIcon icon={faChevronUp} />
+            ) : (
+              <FontAwesomeIcon icon={faChevronDown} />
+            )}
+          </button>
+        </div>
+        {this.state.isOpen ? (
+          <div className="expand__content">{this.props.children}</div>
+        ) : (
+          ''
+        )}
+      </div>
+    );
+  }
+}
+
+export default Expand;
